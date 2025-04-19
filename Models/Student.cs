974dd784ff7aeec707251932c6f836f251e4063b
TheRealJexer <jason.d.dyck@edu.sait.ca>
2025-04-18 19:24:01 -0500

    test

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,8 +3,8 @@ namespace WITWebApp.Models
     public class Student
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Program { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Program { get; set; }
     }
 }
