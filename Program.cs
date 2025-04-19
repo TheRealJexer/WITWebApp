4f52263975527852b396749726763df31feb9e24
TheRealJexer <jason.d.dyck@edu.sait.ca>
2025-04-19 03:04:27 -0500

    test

--- Program.cs
+++ Program.cs
@@ -1,33 +1,68 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using WITWebApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services
 builder.Services.AddControllersWithViews();
-
-// Configure SQL Database connection
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    //options.UseInMemoryDatabase("StudentDb")); // Use real connection for Azure SQL
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // <-- add this
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Registration}/{action=Register}/{id?}");
 
 app.Run();
+
+
+
+//using Microsoft.EntityFrameworkCore;
+//using WITWebApp;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+// Configure SQL Database connection
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
